7c84bd4c214c6ffbe1781e6530fa85a921739647
eggandwaffles <ad3llcomputer@gmail.com>
2020-08-23 16:02:17 -0400

    Added small letter asset, edited gitignore

--- components/LetterLookup.tsx
+++ components/LetterLookup.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 export function ImageFetch (id: string) {
     switch (id) {
         case "aleph":
-            return require("../assets/images/aleph.png");
+            return require("../assets/images/alephSmall.png");
         default:
             return require('../assets/images/nullImage.png');
     }
