e9af14f67559df79f430fc66f9448004f8c206f5
eggandwaffles <ad3llcomputer@gmail.com>
2020-09-02 16:24:30 -0400

    Finally fixed converter problem

--- components/HebrewLettersConverter.js
+++ components/HebrewLettersConverter.js
@@ -3,16 +3,46 @@ var loops = 0
 var abort = false
 var pulses = 0
 var input = []
-var raw = ( 
+var formattedOutput = []
+/*var raw = ( 
 'hebrewLetters[\"Aleph\"]   = \"\u{05D0}\"' +
 'hebrewLetters[\"Bet\"]     = \"\u{FB31}\"' +
 'hebrewLetters[\"Vet\"]     = \"\u{05D1}\"' +
 'hebrewLetters[\"Gimel\"]   = \"\u{05D2}\"'
-)
-//console.log(raw.split(''))
+)*/
+var raw =  ('hebrewLetters["Aleph"]   = "\u{05D0}"' +
+'hebrewLetters["Bet"]     = "\u{FB31}"' +
+'hebrewLetters["Vet"]     = "\u{05D1}"' +
+'hebrewLetters["Gimel"]   = "\u{05D2}"' +
+'hebrewLetters["Dalet"]   = "\u{05D3}"' +
+'hebrewLetters["He"]      = "\u{05D4}"' +
+'hebrewLetters["Vav"]     = "\u{05D5}"' +
+'hebrewLetters["Zayin"]   = "\u{05D6}"' +
+'hebrewLetters["Het"]     = "\u{05D7}"' +
+'hebrewLetters["Tet"]     = "\u{05D8}"' +
+'hebrewLetters["Yud"]     = "\u{05D9}"' +
+'hebrewLetters["Kaf"]     = "\u{05DB}"' +
+'hebrewLetters["Lamed"]   = "\u{05DC}"' +
+'hebrewLetters["Mem"]     = "\u{05DE}"' +
+'hebrewLetters["Nun"]     = "\u{05E0}"' +
+'hebrewLetters["Samech"]  = "\u{05E1}"' +
+'hebrewLetters["Ayin"]    = "\u{05E2}"' +
+'hebrewLetters["Pay"]      = "\u{FB44}"' +
+'hebrewLetters["Phe"]      = "\u{05E4}"' +
+'hebrewLetters["Tsadi"]   = "\u{05E6}"' +
+'hebrewLetters["Kof"]     = "\u{05E7}"' +
+'hebrewLetters["Resh"]    = "\u{05E8}"' +
+'hebrewLetters["Shin"]    = "\u{0FB2A}"' +
+'hebrewLetters["Sin"]     = "\u{0FB2D}"' +
+'hebrewLetters["Tav"]     = "\u{05EA}"' +
+'hebrewLetters["F-Nun"]   = "\u{05DF}"' +
+'hebrewLetters["F-Mem"]   = "\u{05DD}"' +
+'hebrewLetters["F-Kaf"]   = "\u{05DA}\u{05B0}"' +
+'hebrewLetters[""]        = ""' +
+'hebrewLetters[" "]       = ""' +
+'hebrewLetters["Vav-Dagesh"] = "\u{FB35}"')
 var input = (raw.split(''))
 var archiveInput = (raw.split(''))
-console.log(archiveInput)
 var output = [] 
 
 function Delay (milliseconds) {
@@ -22,15 +52,18 @@ function Delay (milliseconds) {
     }
 }
 
-function Scanner (scanning, trigger, debug) {
+function Scanner (scanning, trigger, debug, catchTrig) {
     console.log('Scan Begun')
     var i = 1
     var record = []
     while (i=1) {
         console.log(`First letter is ${scanning[0]}`)
-        if (scanning[0] = trigger) {
-            console.log(scanning)
-            record.push(scanning[0])
+        if (scanning[0] === trigger) {
+            if (catchTrig) {
+                record.push(scanning[0])
+            }
+            
+            console.log(`Record is ${record}`)
             var dump = [record.join(''), scanning.length]
             record = []
             scanning.splice(0, 1);
@@ -41,35 +74,47 @@ function Scanner (scanning, trigger, debug) {
         } else {
             console.log('Scan Pulse')
             debug ++
-            record.push(scanning[1])
+            record.push(scanning[0])
             scanning.splice(0, 1);
         }
     }
 
 }
 
-Delay(2000)
-
 while (input.length > 0 && !abort) {
-    temp = Scanner(input, '"', pulses)
-//    input.splice(0, input.length - temp[1])
-    temp = Scanner(input, '"', pulses)
+    temp = Scanner(input, '"', pulses, true)
+
+    temp = Scanner(input, '"', pulses, false)
     output.push(temp[0])
-//    input.splice(0, input.length - temp[1])
+    var letterName = temp[0]
+    
+    temp = Scanner(input, '"', pulses, true)
+
+    temp = Scanner(input, '"', pulses, false)
     output.push(temp[0])
+    var letterChar = temp[0]
+    if(formattedOutput.length > 0) {
+        formattedOutput.push(`,\n{\n` +
+        `"name" : "${letterName}",\n` +
+        `"char" : "${letterChar}"\n` +
+        `}`
+        )
+    } else {
+        formattedOutput.push(`{\n` +
+        `"name" : "${letterName}",\n` +
+        `"char" : "${letterChar}"\n` +
+        `}`
+        )
+    }
+
+    
+    
     console.log(loops)
     loops ++
     if (loops > 1769) { 
         console.log('FRUIT LOOPS!')
         abort = true
     }
-    console.log(input)
 }
-//console.log(`Raw input was ${raw}`)
-//Delay(2000)
-//console.log(`which was parsed to ${archiveInput}`)
-//Delay(2000)
-//console.log(`The derived result is ${output.join('')}`)
-//Delay(2000)
-console.log(`${pulses} pulses were completed`)
-console.log(output.join(''))
+
+console.log("[\n" + formattedOutput.join('') + "\n]")
