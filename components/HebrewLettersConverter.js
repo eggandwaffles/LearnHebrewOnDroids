db488ab960e6c9466995582d0d070baf4364f1f3
eggandwaffles <ad3llcomputer@gmail.com>
2020-09-17 18:38:24 -0400

    Updated converter

--- components/HebrewLettersConverter.js
+++ components/HebrewLettersConverter.js
@@ -1,43 +1,13 @@
+//This is a tool to convert data from the format found in the iOS app to a JSON format. It takes input from a file name toConvert.txt placed at this level.
+//JSON is output to the ConverterOutput.json file placed at this same level. This program cost me a lot of time but gosh it's useful
 var temp = []
 var loops = 0
 var abort = false
 var pulses = 0
 var input = []
 var formattedOutput = []
-
 var fs = require('fs')
-/* var raw =  ('hebrewLetters["Aleph"]   = "\u{05D0}"' +
-'hebrewLetters["Bet"]     = "\u{FB31}"' +
-'hebrewLetters["Vet"]     = "\u{05D1}"' +
-'hebrewLetters["Gimel"]   = "\u{05D2}"' +
-'hebrewLetters["Dalet"]   = "\u{05D3}"' +
-'hebrewLetters["He"]      = "\u{05D4}"' +
-'hebrewLetters["Vav"]     = "\u{05D5}"' +
-'hebrewLetters["Zayin"]   = "\u{05D6}"' +
-'hebrewLetters["Het"]     = "\u{05D7}"' +
-'hebrewLetters["Tet"]     = "\u{05D8}"' +
-'hebrewLetters["Yud"]     = "\u{05D9}"' +
-'hebrewLetters["Kaf"]     = "\u{05DB}"' +
-'hebrewLetters["Lamed"]   = "\u{05DC}"' +
-'hebrewLetters["Mem"]     = "\u{05DE}"' +
-'hebrewLetters["Nun"]     = "\u{05E0}"' +
-'hebrewLetters["Samech"]  = "\u{05E1}"' +
-'hebrewLetters["Ayin"]    = "\u{05E2}"' +
-'hebrewLetters["Pay"]      = "\u{FB44}"' +
-'hebrewLetters["Phe"]      = "\u{05E4}"' +
-'hebrewLetters["Tsadi"]   = "\u{05E6}"' +
-'hebrewLetters["Kof"]     = "\u{05E7}"' +
-'hebrewLetters["Resh"]    = "\u{05E8}"' +
-'hebrewLetters["Shin"]    = "\u{0FB2A}"' +
-'hebrewLetters["Sin"]     = "\u{0FB2D}"' +
-'hebrewLetters["Tav"]     = "\u{05EA}"' +
-'hebrewLetters["F-Nun"]   = "\u{05DF}"' +
-'hebrewLetters["F-Mem"]   = "\u{05DD}"' +
-'hebrewLetters["F-Kaf"]   = "\u{05DA}\u{05B0}"' +
-'hebrewLetters[""]        = ""' +
-'hebrewLetters[" "]       = ""' +
-'hebrewLetters["Vav-Dagesh"] = "\u{FB35}"')
-*/
+
 function Delay (milliseconds) {
     var timeEnd = Date.now() + milliseconds
     while (Date.now() < timeEnd) {
@@ -123,5 +93,7 @@ while (input.length > 0 && !abort) {
         abort = true
     }
 }
-
-console.log("[\n" + formattedOutput.join('') + "\n]")
+fs.writeFile('./ConverterOutput.json', "[\n" + formattedOutput.join('') + "\n]", "utf8", () => {
+    console.log("Done writing to file")
+})
+//console.log("[\n" + formattedOutput.join('') + "\n]")
