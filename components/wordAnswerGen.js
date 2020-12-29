0ec0b4c30d1f01620906bbc85d958a3464078c23
eggandwaffles <ad3llcomputer@gmail.com>
2020-12-28 21:10:30 -0500

    Continued work on word game

--- components/wordAnswerGen.js
+++ components/wordAnswerGen.js
@@ -1,3 +1,4 @@
+//const { resolveUri } = require("expo-asset/build/AssetSources");
 const { resolveTypeReferenceDirective } = require("typescript");
 var wordData = require("../assets/wordData.json");
 var { RandInt } = require("./RandInt.js")
@@ -10,9 +11,11 @@ function catWords (categories) {
     for (let i = 0; i < wordData.length; i++) {
         var doesFit = false
         for (let i1 = 0; i1 < categories.length; i1++) {
-            if (wordData[i].category.includes(wordcategories[i1])) {
+            if (wordData[i].categories.includes(categories[i1])) {
                 doesFit = true
+                //console.log("A matching word has been found!")
             }
+            
         }
         if (doesFit) {
             WordsInCat.push(wordData[i])
@@ -23,18 +26,75 @@ function catWords (categories) {
 //Function 2: pick a word, any word
 function wheelOfMisfortune (categories) {
     var pieSlices = catWords(categories)
-    return pieSlices[RandInt(0, pieSlices-1)]
+    return pieSlices[RandInt(0, pieSlices.length-1)]
 }
 //Function 3: shuffle an array
-function shuffleArr (UnshuffledArr, length) {
-    var pool = UnshuffledArr
-    if (length > UnshuffledArr.length) {
+function shuffleArr (unshuffledArr, length) {
+    var pool = [...unshuffledArr]
+    //console.log("Pool initialized as " + pool)
+    if (length > unshuffledArr.length) {
+        console.error("ERROR: requested length exceeds source length.")
         return pool
+        
     }
     var shuffled = []
-    for (let i2 = 0; i2 < length; i++) {
-        var choice = pool.splice(RandInt(0, pool.length - 1))
-        shuffled.push(choice)
+    for (let i2 = 0; i2 < length; i2++) {
+        var choice = pool.splice(RandInt(0, pool.length - 1), 1)
+        shuffled.push(choice.join(""))
+        //console.log("Pool is now " + pool + ". Shuffled is now " + shuffled + ".")
     }
     return shuffled
 }
+//Interlude 1: standard template for word questions
+const AnswerContainer = {
+    "letters" : "",
+    "TranslitOptions" : [],
+    "CorrectTranslit" : "",
+    "TranslateOptions" : [],
+    "CorrectTranslate" : ""
+}
+
+//Interlude 2: Testing function
+function testFuncs (reps) {
+    var arcReps = reps
+    var results = []
+    
+    //build data
+    while (reps>0) {
+        var midres = ({
+            "input" : null, 
+            "output" : null
+        })
+        
+        var testArr = wheelOfMisfortune((["intro", "wine"])).close_transliterations
+        midres.input = testArr
+        var shufTest = shuffleArr(testArr, 5)
+        midres.output = shufTest
+        results.push(midres)
+        reps--
+    }
+    //test data
+    var matches = {
+        "Place1" : 0,
+        "Place2" : 0,
+        "Place3" : 0,
+        "Place4" : 0,
+        "Place5" : 0
+    }
+    for (let tI = 0; tI < results.length; tI++) {
+        if(results[tI].input[0] === results[tI].output[0]) {matches.Place1++}
+        if(results[tI].input[1] === results[tI].output[1]) {matches.Place2++}
+        if(results[tI].input[2] === results[tI].output[2]) {matches.Place3++}
+        if(results[tI].input[3] === results[tI].output[3]) {matches.Place4++}
+        if(results[tI].input[4] === results[tI].output[4]) {matches.Place5++}
+    }
+    function percentaverage(datasum, trials) {
+        return (((datasum/trials)*100) + "%")
+    }
+    console.log("Frequencies are:/=\nPlace 1: " + percentaverage(matches.Place1, arcReps) + "\nPlace 2: " + percentaverage(matches.Place2, arcReps) + "\nPlace 3: " + percentaverage(matches.Place3, arcReps) + "\nPlace 4: " + percentaverage(matches.Place4, arcReps) + "\nPlace 5: " + percentaverage(matches.Place5, arcReps) + "\nAnalysis Complete!")
+}
+//Function 4: inject a correct answer and format an AnswerContainer
+function FinalAnswer (categories) { 
+
+}
+testFuncs(1000000)
